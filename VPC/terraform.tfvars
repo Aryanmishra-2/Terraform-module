7eca68813cdd9eb551f0f5a3df9b5f2c89089c05
Aryanmishra-2 <aryan.mishra@mygurukulam.co>
2026-02-04 12:25:48 +0530

    Add files via upload

---
+++ VPC/terraform.tfvars
@@ -0,0 +1,11 @@
+region = "ap-south-1"
+
+vpc_name = "demo-vpc"
+vpc_cidr = "10.0.0.0/16"
+
+az = "ap-south-1a"
+
+public_subnet_cidr  = "10.0.1.0/24"
+private_subnet_cidr = "10.0.2.0/24"
+db_subnet_cidr      = "10.0.3.0/24"
+
---
+++ VPC/variables.tf
@@ -0,0 +1,35 @@
+variable "region" {
+  type    = string
+  default = ""
+}
+
+variable "vpc_name" {
+  type    = string
+  default = ""
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = ""
+}
+
+variable "az" {
+  type    = string
+  default = ""
+}
+
+variable "public_subnet_cidr" {
+  type    = string
+  default = ""
+}
+
+variable "private_subnet_cidr" {
+  type    = string
+  default = ""
+}
+
+variable "db_subnet_cidr" {
+  type    = string
+  default = ""
+}
+
